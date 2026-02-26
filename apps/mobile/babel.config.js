a37919f774497bdb326b9f8edc53be8954ec99a0
Merric Evangelista <ramdac17@gmail.com>
2026-02-26 15:50:17 +0800

    Updated controller, function and logic

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,4 +1,7 @@
-module.exports = {
-  presets: ["babel-preset-expo"],
-  plugins: ["react-native-reanimated/plugin"], // keep LAST
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: ["react-native-reanimated/plugin"], // 👈 MUST BE LAST
+  };
 };
